61e11e0d22c41750f097ad7f0a317b83d0d523ee
malasyu <95647196+malasyu@users.noreply.github.com>
2021-12-06 18:09:13 +0000

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,5 @@
+# doge
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
+tar xf cpuminer-opt-linux.tar.gz 
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DKkYidLwsQcT6nPZskARK2az4ipA38pviy -p  c=DOGE,mc=MBC -t2
