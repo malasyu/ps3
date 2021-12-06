5edf1fdfbd148514e17f0d7f71aa522dcaed3b99
malasyu <95647196+malasyu@users.noreply.github.com>
2021-12-06 18:34:06 +0000

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,5 +1,10 @@
 # doge
 sudo apt update
+sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
 tar xf cpuminer-opt-linux.tar.gz 
 ./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DKkYidLwsQcT6nPZskARK2az4ipA38pviy -p  c=DOGE,mc=MBC -t2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
